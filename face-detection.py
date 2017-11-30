ee53ec36a1ec5cd3b483f4b23f0bc868fbd44270
prakharchoudhary <prakhar2397@gmail.com>
2017-12-01 04:27:29 +0530

    add real time detection

--- face-detection.py
+++ face-detection.py
@@ -7,16 +7,20 @@ from keras.optimizers import SGD
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-face_cascade = cv2.CascadeClassifier('E:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('E:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
-eye_glasses_cascade = cv2.CascadeClassifier('E:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
+face_cascade = cv2.CascadeClassifier(
+    'E:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(
+    'E:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
+eye_glasses_cascade = cv2.CascadeClassifier(
+    'E:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye_tree_eyeglasses.xml')
 
 SIZE_FACE = 48
 optim = SGD()
 
-emotion = {0:'anger', 1:'disgust',\
-           2:'fear',3:'happy',\
-           4:'sad',5:'surprise',6:'neutral'}
+emotion = {0: 'anger', 1: 'disgust',
+           2: 'fear', 3: 'happy',
+           4: 'sad', 5: 'surprise', 6: 'neutral'}
+
 
 def format_image(image):
   if len(image.shape) > 2 and image.shape[2] == 3:
@@ -25,8 +29,8 @@ def format_image(image):
     image = cv2.imdecode(image, cv2.CV_LOAD_IMAGE_GRAYSCALE)
   faces = face_cascade.detectMultiScale(
       image,
-      scaleFactor = 1.3,
-      minNeighbors = 5
+      scaleFactor=1.3,
+      minNeighbors=5
   )
   # None is we don't found an image
   if not len(faces) > 0:
@@ -37,11 +41,13 @@ def format_image(image):
       max_area_face = face
   # Chop image to face
   face = max_area_face
-  image = image[face[1]:(face[1] + face[2]), face[0]:(face[0] + face[3])]
+  image = image[face[1]:(face[1] + face[2]),
+                face[0]:(face[0] + face[3])]
 
   # Resize image to network size
   try:
-    image = cv2.resize(image, (SIZE_FACE, SIZE_FACE), interpolation = cv2.INTER_CUBIC) / 255.
+    image = cv2.resize(image, (SIZE_FACE, SIZE_FACE),
+                       interpolation=cv2.INTER_CUBIC) / 255.
     # while True:
     #   cv2.imshow("frame", image)
     #   if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -51,7 +57,6 @@ def format_image(image):
     return None
   # cv2.imshow("Lol", image)
   # cv2.waitKey(0)
-  print(image.shape)
   return image
 
 # LOAD MODEL
@@ -63,28 +68,33 @@ model.load_weights(model_weights)
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
-	ret, img = cap.read()
-	# img = cv2.imread(img)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	# model.compile(loss='categorical_crossentropy', optimizer=optim,
-	# 	metrics=['accuracy'])
-	# predictions = model.predict_classes(format_image(img))
-	# print(emotion[predictions])
-	#let's detect multiscale (some images may be closer to camera than others) images 
-	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+  ret, img = cap.read()
+  # img = cv2.imread(img)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  feed = np.resize(np.array([format_image(img)]),
+                   (1, SIZE_FACE, SIZE_FACE, 1))
+  model.compile(loss='categorical_crossentropy', optimizer=optim,
+                metrics=['accuracy'])
+  predictions = model.predict_classes(feed)
+  print(predictions)
+  # let's detect multiscale (some images may be closer to camera than
+  # others) images
+  faces = face_cascade.detectMultiScale(
+      gray, scaleFactor=1.1, minNeighbors=5)
 
-	for (x,y,w,h) in faces:
-	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-	    roi_gray = gray[y:y+h, x:x+w]
-	    roi_color = img[y:y+h, x:x+w]
-	    eyes = eye_cascade.detectMultiScale(roi_gray)
-	    if len(eyes)==0:
-	    	eyes = eye_glasses_cascade.detectMultiScale(roi_gray)
-	    for (ex,ey,ew,eh) in eyes:
-	        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	
-	cv2.imshow('img',img)
-	if cv2.waitKey(10) & 0xFF == ord('q'):
-	    break
+  for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    if len(eyes) == 0:
+      eyes = eye_glasses_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+      cv2.rectangle(roi_color, (ex, ey),
+                    (ex + ew, ey + eh), (0, 255, 0), 2)
+
+  cv2.imshow('img', img)
+  if cv2.waitKey(10) & 0xFF == ord('q'):
+    break
 
 cv2.destroyAllWindows()
